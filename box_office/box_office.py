d5c095a7604565a6c68b6f7e0b2f12443bc3ea11
James Blackwell <jblackwell817@gmail.com>
2019-10-12 22:01:54 +0100

    clean data from function

--- box_office/box_office.py
+++ box_office/box_office.py
@@ -1,13 +1,13 @@
-import pandas as pd
-import numpy as np
-
-#get training data
-training_data = pd.read_csv('resources/train.csv', index_col="id")
-#to start with use the following fields, as a guess:
-#budget, genre, language, popularity, production company, release date
+#import the data
+from clean_data import clean_data
+path_to_csv = 'resources/train.csv'
 col_names = ["budget","genres","original_language","popularity","production_companies","release_date","revenue"]
-reduced_data = training_data[col_names]
-#only use entries which don't contain null values
-complete_data = reduced_data.dropna()
-print(complete_data.head)
+index = 'id'
 
+clean_data = clean_data(path_to_csv,col_names,index)
+complete_data = clean_data[clean_data.budget != 0]
+training_data = complete_data.drop('revenue',axis=1)
+training_output = complete_data['revenue']
+
+#print(training_data.head)
+#print(training_output.head)
